0e8ff954d5cb97317a31e1bf2844008ddc627cbb
GUILHERME NOGUEIRA FERREIRA <guilherme.nferreira2@senacsp.edu.br>
2019-09-27 22:07:38 -0300

    27/09

--- src/app/pedidos/shared/carrinho.service.ts
+++ src/app/pedidos/shared/carrinho.service.ts
@@ -10,20 +10,62 @@ import { map } from 'rxjs/operators';
 export class CarrinhoService {
 
   constructor(private db: AngularFireDatabase, private afAuth: AngularFireAuth) { }
-  
+
   getCarrinhoProdutosRef(){
     const path = `${FirebasePath.CARRINHO}${this.afAuth.auth.currentUser.uid}/${FirebasePath.PRODUTOS}`;
-  return this.db.list(path);
+    return this.db.list(path);
   }
   insert(itemProduto: any){
-    return this.getCarrinhoProdutosRef().push(itemProduto)
+    return this.getCarrinhoProdutosRef().push(itemProduto);
   }
 
   carrinhoPossuiItens(){
   return this.getCarrinhoProdutosRef().snapshotChanges().pipe(
     map(changes => {
-      return changes.length > 0
+      return changes.length > 0;
      })
     )
   }
+
+calcularTotal(preco: number, quantidade: number){
+  return preco * quantidade;
+
 }
+
+update(key: string, quantidade: number, total: number){
+return this.getCarrinhoProdutosRef().update(key, {quantidade: quantidade, total: total});
+}
+
+remove(key: string){
+return this.getCarrinhoProdutosRef().remove(key);
+}
+
+getAll(){
+return this.getCarrinhoProdutosRef().snapshotChanges().pipe(
+  map(changes => {
+    return changes.map(m => ({key: m.payload.key, ...m.payload.val() }))
+   })
+  )
+}
+
+getTotalPedido(){
+return this.getCarrinhoProdutosRef().snapshotChanges().pipe(
+  map(changes => {
+    return changes
+    .map( (m: any) => (m.payload.val().total))
+    .reduce( (prev: number, current: number) => {
+      return prev + current;
+    })
+  })
+ )
+}
+
+clear(){
+
+}
+
+
+
+
+}
+
